a30e2ea80def53dd8eebdf72ded891e15452fb6c
Ricky Zaman <rzaman6@myseneca.ca>
2019-03-28 22:35:19 -0400

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@
  * assignment has been copied manually or electronically from any other source (including web sites) or 
  * distributed to other students.  
  * Name: Ricky Zaman   Student ID: 121942171   Date: March 28, 2019
- * Heroku Link: _______________________________________________________________ * 
+ * Heroku Link: _______________________________________________________________ 
  * ********************************************************************************/
 
 import { Component } from '@angular/core';
